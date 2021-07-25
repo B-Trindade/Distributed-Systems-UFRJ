ad4facaef0c6b2827f20be26130bc1d6b4a7f900
Breno Trindade <bttostes@dcc.ufrj.br>
2021-07-25 14:57:10 -0300

    New: error handling + send multiple messages

--- Lab 1/ativo.py
+++ Lab 1/ativo.py
@@ -5,13 +5,40 @@ import socket
 HOST = 'localhost'
 PORT = 8000
 
-sckt = socket.socket()
+# Cria o objeto socket
+try:
+    sckt = socket.socket()
+except socket.error as e:
+    print('Erro durante criacao do socket %s' % e)
+    raise SystemExit #sys.exit(1) mas sem o import
 
-sckt.connect((HOST, PORT))
+# Estabelece conexao entre sockets
+try:
+    sckt.connect((HOST, PORT))
+except socket.gaierror as e:
+    print('Falha durante tentativa de conectar com: %s' % e)
+    raise SystemExit
+except socket.error as e:
+    print('Erro de conexao: %s' % e)
+    raise SystemExit
 
-sckt.send(b'Ola sou o lado ativo!')
+print('Conectado com servidor de echo (%s) pela porta %s.' % (HOST,PORT))
+print('Para encerrar a conexao digite \'exit\'.\n')
 
-msg = sckt.recv(1024)
-print(str(msg, encoding='utf-8'))
+# bloco try-except para envio e recebimento da mensagem de echo
+try:
+    while True:
+        msg = input('Insira sua mensagem: ')
+        if msg == 'exit':
+            print('Encerrando conexao...')
+            break
+        sckt.send(msg.encode('utf-8'))
 
-sckt.close()
+        echo = sckt.recv(1024)
+        print('Echo> ' + str(echo, encoding='utf-8'))
+        print()
+except socket.error as e:
+    print('Error durante envio/recebimento de mensagem: %s' % e)
+    raise SystemExit
+finally:
+    sckt.close()
