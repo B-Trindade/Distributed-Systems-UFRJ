963eb7cdca39c2c9ac262942e952689329b88cec
Breno Trindade <bttostes@dcc.ufrj.br>
2021-08-02 01:45:17 -0300

    New: test file + server and client

--- Lab 2/passivo.py
+++ Lab 2/passivo.py
@@ -3,7 +3,17 @@
 import socket
 
 HOST = ''
-PORT = 8000
+PORT = 5000
+ENCODING = 'utf-8'
+
+#Recebe o filename e tenta abri-lo como read only
+def get_file(file_name: str):
+    try:
+        with open(file_name, 'r') as file:
+            data = file.read()
+            return data
+    except:
+        return None
 
 # Descritor socket
 sckt = socket.socket() # default: socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,17 +21,38 @@ sckt = socket.socket() # default: socket.socket(socket.AF_INET, socket.SOCK_STRE
 # Vincular endereco e porta
 sckt.bind((HOST, PORT))
 
+# Escuta apenas um client por vez
 sckt.listen(1)
 
-novoSckt, endereco = sckt.accept()
-print('Conectado com: ' + str(endereco))
 
 while True:
-    msg = novoSckt.recv(1024)
-    if not msg: break
-    print('Mensagem recebida: ' + str(msg, encoding='utf-8'))
-    novoSckt.send(msg)
+    novoSckt, endereco = sckt.accept()
+    print('Conectado com: ' + str(endereco))
 
-novoSckt.close()
+    while True:
+        file_name = novoSckt.recv(1024)
+        if not file_name: break
+        file_name = str(file_name, encoding=ENCODING)
 
+        data = get_file(file_name)
+        if data is not None:
+            msg = 'Arquivo encontrado com sucesso!'
+            print('Enviando mensgem de sucesso...')
+            novoSckt.send(msg.encode(ENCODING))
+
+            search_query = novoSckt.recv(1024)
+            if not file_name: break
+            search_query = str(search_query, encoding=ENCODING)
+
+            word_count = data.count(search_query)
+            response = f'O arquivo "{file_name}" possui um total de {word_count} instancias da palavra "{search_query}".'
+            novoSckt.send(response.encode(ENCODING))
+
+            print('Resposta para client enviada.')
+        else:
+            err_msg = 'Falha: Arquivo nao encontrado.'
+            print('Enviando mensagem de falha...')
+            novoSckt.send(err_msg.encode(ENCODING))
+            
+    novoSckt.close()
 sckt.close()
